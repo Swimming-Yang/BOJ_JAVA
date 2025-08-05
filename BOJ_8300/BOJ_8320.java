42a0161b80da76175627096d051dd6a6484031bd
Swimming_Yang <ysw1mst@gmail.com>
2025-08-05 10:21:49 +0900

    Update BOJ_8320.java

--- BOJ_8300/BOJ_8320.java
+++ BOJ_8300/BOJ_8320.java
@@ -1,5 +1,25 @@
-public class BOJ_8320
+//BOJ_8320
 
-    public static void main(String[] args) {
-    Buffered
+import java.io.*;
+
+
+public class BOJ_8320 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int count = 0;
+
+        for(int i = 1; i <= n; i++) {
+            for(int j = i; i * j <= n; j++) {
+                count ++;
+            }
+        }
+
+        System.out.println(count);
+        br.close();
+
+
     }
+
+}
