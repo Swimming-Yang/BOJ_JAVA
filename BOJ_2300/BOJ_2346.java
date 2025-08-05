29ea488db3e2c02aa9e3177da5e7565dca1499b4
Swimming_Yang <ysw1mst@gmail.com>
2025-08-05 17:47:49 +0900

    Update BOJ_2346.java
    1

--- BOJ_2300/BOJ_2346.java
+++ BOJ_2300/BOJ_2346.java
@@ -1,5 +1,47 @@
 package BOJ_2300;
 
-public class BOJ_2346 {
+import java.io.*;
+import java.util.*;
 
+public class BOJ_2346 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int arr_length = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        LinkedList<Integer> balloons = new LinkedList<>();
+        for (int i = 1; i <= arr_length; i++) {
+            balloons.add(i);
+        }
+        
+        int[] papers = new int[arr_length];
+        for (int i = 0; i < arr_length; i++) {
+            papers[i] = Integer.parseInt(st.nextToken());
+        }
+
+        StringBuilder result = new StringBuilder();
+        int currentIndex = 0;
+
+        for (int j = 0; j < arr_length; j++) {
+
+            int currentBalloon = balloons.remove(currentIndex);
+            result.append(currentBalloon).append(" ");
+            
+            if (balloons.isEmpty()) break;
+            
+            int move = papers[currentBalloon - 1];
+            
+            if (move > 0) {
+                currentIndex = (currentIndex + move - 1) % balloons.size();
+            } else {  
+                currentIndex = (currentIndex + move) % balloons.size();
+                if (currentIndex < 0) {
+                    currentIndex += balloons.size();
+                }
+            }
+        }
+        
+        System.out.println(result.toString().trim()); //trim() == 앞 뒤 공백 제거
+    }
 }
