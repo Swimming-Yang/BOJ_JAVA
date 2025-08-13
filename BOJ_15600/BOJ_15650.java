b7eb8e44734aaff90ad78d70f0ca7b64ba1f3cf9
Swimming_Yang <ysw1mst@gmail.com>
2025-08-13 16:27:35 +0900

    Create BOJ_15650.java
    11

---
+++ BOJ_15600/BOJ_15650.java
@@ -0,0 +1,39 @@
+//BOJ_15650_N과M;
+
+package BOJ_15600;
+
+import java.io.*;
+import java.util.*;
+
+
+public class BOJ_15650 {
+
+    static boolean[] visited;
+    static List<List<Integer>> result = new ArrayList<>();
+    static int[] arr;
+    static List<Integer> current;
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException{
+        
+        st = new StringTokenizer(br.readLine());
+        
+        int N = Integer.parseInt(st.nextToken());
+        int R = Integer.parseInt(st.nextToken());
+
+        //초기화
+        visited = new boolean[N];
+        Lis
+
+
+
+    }
+
+
+    public static void permutation (){
+
+    }
+
+}
