44bbc5e3f2673127dfd6a60a03d924552c19dd98
Kim Doyoung <ysw1mst@gmail.com>
2025-08-21 16:13:27 +0900

    123
    123

--- prc.java
+++ prc.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-public class Prac {
+public class prc {
 
     public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     public static StringTokenizer st;
@@ -14,37 +14,36 @@ public class Prac {
 
     public static int size;
 
-public static void main(String[] args) throws IOException{
-    int testcase_num = Integer.parseInt(br.readLine());
-    size = Integer.parseInt(br.readLine());
-    for(int testcase = 1; testcase <= testcase_num; testcase++) {
-    //초기화
-    graph = new int[size][size];
-    distance = new int[size][size];
+    public static void main(String[] args) throws IOException{
+        int testcase_num = Integer.parseInt(br.readLine());
+        
+        for(int testcase = 1; testcase <= testcase_num; testcase++) {
+            size = Integer.parseInt(br.readLine());
+            
+            //초기화
+            graph = new int[size][size];
+            distance = new int[size][size];
 
-    for(int i = 0; i < size; i++) {
-        for(int j = 0; j < size; j++) {
-            distance[i][j] = Integer.MAX_VALUE;
+            for(int i = 0; i < size; i++) {
+                for(int j = 0; j < size; j++) {
+                    distance[i][j] = Integer.MAX_VALUE;
+                }
+            }
+
+            for(int i = 0; i < size; i++) {
+                st = new StringTokenizer(br.readLine());
+                for(int j = 0; j < size; j++) {
+                    graph[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
+
+            dijkstra(0, 0);
+
+            System.out.println("#" + testcase + " " + distance[size - 1][size - 1]);
         }
     }
 
-    //Arrays.fill(distance, Integer.MAX_VALUE);
-    
-
-    for(int i = 0; i < size; i++) {
-        st = new StringTokenizer(br.readLine());
-        for(int j = 0; j < size; j++) {
-            graph[i][j] = Integer.parseInt(st.nextToken());
-        }
-    }
-
-    dijkstra(0, 0);
-
-    System.out.println("#" + testcase_num + " " + distance[size - 1][size - 1]);
-        }
-    }
-
-        static class Node implements Comparable<Node> {
+    static class Node implements Comparable<Node> {
         int x;
         int y; //도착 정점
         int weight; //가중치
@@ -60,9 +59,8 @@ public static void main(String[] args) throws IOException{
             return Integer.compare(this.weight, other.weight);
         }
     }
-    
 
-        public static void dijkstra(int start_x, int start_y) {
+    public static void dijkstra(int start_x, int start_y) {
         //우선순위 큐 사용 (가중치가 작은 순서대로 자동 정렬)
         PriorityQueue<Node> pq = new PriorityQueue<>();
 
@@ -83,9 +81,9 @@ public static void main(String[] args) throws IOException{
                 int nx = current.x + dx[i];
                 int ny = current.y + dy[i];
 
-                int nextWeight = current.weight;
-
                 if(nx >= 0 && nx < size && ny >= 0 && ny < size) {
+                    int nextWeight = current.weight;
+
                     //가중치가 같을 때
                     if(graph[current.x][current.y] == graph[nx][ny]) {
                         nextWeight += 1;
@@ -94,15 +92,14 @@ public static void main(String[] args) throws IOException{
                     else if (graph[current.x][current.y] < graph[nx][ny]) {
                         nextWeight += ((graph[nx][ny] - graph[current.x][current.y]) * 2);
                     }
-                    //nx,y의 가중치가 작을때
-                    //알빠아님
+                    
 
-                    distance[nx][ny] = Math.min(distance[nx][ny], nextWeight);
-
-                    pq.offer(new Node(nx, ny, nextWeight));
+                    if (nextWeight < distance[nx][ny]) {
+                        distance[nx][ny] = nextWeight;
+                        pq.offer(new Node(nx, ny, nextWeight));
+                    }
                 }
             }
-
         }
     }
 }
