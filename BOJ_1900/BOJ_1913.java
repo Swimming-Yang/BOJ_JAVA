e72cecc48fe34b7fb404705a12f526b27e42f909
Kim Doyoung <ysw1mst@gmail.com>
2025-08-28 17:53:00 +0900

    Create BOJ_1913.java

---
+++ BOJ_1900/BOJ_1913.java
@@ -0,0 +1,30 @@
+package BOJ_1900;
+
+import java.io.*;
+import java.util.*;
+
+public class BOJ_1913 {
+
+    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static StringTokenizer st;
+
+    public static int[][] arr;
+    public static int dx[] = {0, 1, 0, -1};
+    public static int dy[] = {1, 0, -1, 0};
+
+    public static void main(String[] args) throws IOException{
+        int num = Integer.parseInt(br.readLine());
+        int target_num = Integer.parseInt(br.readLine());
+
+        arr = new int[num][num];
+        int first_location = arr[(num / 2) + 1][(num / 2) + 1];
+
+        for(int i = 1; i <= Math.pow(num, 2); i++) {
+                first_location = 1;
+
+            }
+        }
+
+
+    }
+
