1bec7833024887ec74c8ada7ac2aaa35d79f8c83
Kim Doyoung <ysw1mst@gmail.com>
2025-09-01 12:55:54 +0900

    Create BOJ_20040.java

---
+++ BOJ_20000/BOJ_20040.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class BOJ_20040 {
+
+    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static StringTokenizer st;
+    public static void main(String[] args) throws IOException{
+        
+        st = new StringTokenizer(br.readLine());
+
+        
+    }
+
+}
