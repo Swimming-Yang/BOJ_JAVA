2872104ca870c8097a42c9b8f54be7dfd9fbad1d
Swimming-Yang <ysw1mst@gmail.com>
2025-09-10 21:50:10 +0900

    Update practice.java

--- practice.java
+++ practice.java
@@ -2,62 +2,42 @@ import java.io.*;
 import java.util.*;
 
 public class practice {
-
-    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    public static StringTokenizer st;
+    public static int[] dr = {-1, 0, 1, 0};
+    public static int[] dc = {0, 1, 0, -1};
 
     public static int[][] map;
-    public static int white;
-    public static int blue;
+    public static boolean[][] visited;
 
+    public static int answer;
     public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
+        int testcase_num = Integer.parseInt(br.readLine());
 
-        //*첫째 줄에는 종이 한변의 길이 N이 주어짐 
-        int size = Integer.parseInt(br.readLine());
-
-        map = new int[size][size];
-
-        for(int i = 0; i < size; i++) {
+        for(int i = 0; i < testcase_num; i++) {
             st = new StringTokenizer(br.readLine());
-            for(int j = 0; j < size; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
+            int row = Integer.parseInt(st.nextToken());
+            int col = Integer.parseInt(st.nextToken());
+            int bechu = Integer.parseInt(st.nextToken());
+
+            map = new int[row][col];
+            visited = new boolean[row][col];
+
+            for(int j = 0; j < bechu; j++) {
+                st = new StringTokenizer(br.readLine());
+                int cr = Integer.parseInt(st.nextToken());
+                int cc = Integer.parseInt(st.nextToken());
+
+                map[cr][cc] = 1;
             }
+
+            bfs(0, 0);
         }
-        divide(0, 0, size);
-        System.out.println(white);
-        System.out.println(blue);
     }
 
-    public static void divide(int x, int y, int size) {
-        if(scan(x, y, size)) {
-            if(map[x][y] == 0) {
-                white++;
-            }
-            else {
-                blue++;
-            }
-            return;
-        }
-
-        //else
-        int newSize = size / 2;
-        divide(x, y, newSize);
-        divide(x, y + newSize, newSize);
-        divide(x + newSize, y, newSize);
-        divide(x + newSize, y + newSize, newSize);
-    }
-
-    public static boolean scan(int x, int y, int size) {
-        int color = map[x][y];
-
-        for(int i = x; i < x + size; i++) {
-            for(int j = y; j < y + size; j++) {
-                if(map[i][j] != color) {
-                return false;
-                }
-            }
-        }
-        return true;
+    public static void bfs(int cur_r, int cur_c) {
+        visited[cur_r][cur_c] = true;
+        
     }
 }
