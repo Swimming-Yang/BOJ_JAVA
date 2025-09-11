0e27116183076d8f7ea6d99c415f46056e8e0811
Swimming-Yang <ysw1mst@gmail.com>
2025-09-11 21:44:13 +0900

    ㅕㅔ

--- BOJ_11400/BOJ_11403.java
+++ BOJ_11400/BOJ_11403.java
@@ -7,44 +7,39 @@ public class BOJ_11403 {
 
     public static int[][] map;
     public static int INF = 100_000_000;
-    
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
 
-        int size = Integer.parseInt(br.readLine());
-        map = new int[size + 1][size + 1];
+        int Node = Integer.parseInt(br.readLine());
+        map = new int[Node][Node];        
 
-        for(int i = 1; i <= size; i++) {
+        for(int row = 0; row < Node; row++) {
             st = new StringTokenizer(br.readLine());
-            for(int j = 1; j <= size; j++) {
-                int value = Integer.parseInt(st.nextToken());
-                if(value == 1) {
-                    map[i][j] = 1;  // 직접 연결된 경우
-                } else {
-                    map[i][j] = INF;  // 연결되지 않은 경우
+            for(int col = 0; col < Node; col++) {
+                map[row][col] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        //*플로이드 - 와샬 */
+        for(int mid = 0; mid < Node; mid++) {
+            for(int str = 0; str < Node; str++) {
+                for(int end = 0; end < Node; end++) {
+                    map[str][end] = Math.min(map[str][end], map[str][mid] + map[mid][end]);
                 }
             }
         }
 
-        // 플로이드 워셜
-        for(int k = 1; k <= size; k++) {
-            for(int i = 1; i <= size; i++) {
-                for(int j = 1; j <= size; j++) {
-                    map[i][j] = Math.min(map[i][j], map[i][k] + map[k][j]);
+        for(int row = 0; row < Node; row++) {
+            for(int col = 0; col < Node; col++) {
+                if(map[row][col] == 0) {
+                    sb.append(0 + " ");
                 }
+                sb.append("\n");
             }
         }
-
-        for(int i = 1; i <= size; i++) {
-            for(int j = 1; j <= size; j++) {
-                if(map[i][j] == INF) {
-                    System.out.print("0 ");
-                } else {
-                    System.out.print("1 ");
-                }
-            }
-            System.out.println();
-        }
+        System.out.println(sb);
+        br.close();
     }
 }
