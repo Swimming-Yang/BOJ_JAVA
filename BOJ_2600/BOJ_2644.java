8488d9f0ddee0c051e81e665c77bfb5bcbbfef0c
Swimming_Yang <ysw1mst@gmail.com>
2025-09-13 14:24:43 +0900

    Update BOJ_2644.java

--- BOJ_2600/BOJ_2644.java
+++ BOJ_2600/BOJ_2644.java
@@ -1,5 +1,14 @@
+//BOJ_2644_촌수계산
+
 package BOJ_2600;
 
+import java.io.*;
+import java.util.*;
+
 public class BOJ_2644 {
+    public static void main(String[] args) throws IOException{
+        
+        // *
+    }
 
 }
