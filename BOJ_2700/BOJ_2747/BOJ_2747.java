1e83bb0af15fe09acc8c16e3a2c70a85de8aed61
Swimming_Yang <ysw1mst@gmail.com>
2025-09-13 18:02:50 +0900

    123

--- BOJ_2700/BOJ_2747/BOJ_2747.java
+++ BOJ_2700/BOJ_2747/BOJ_2747.java
@@ -1,22 +1,26 @@
-package BOJ_2747;
+package BOJ_2700.BOJ_2747;
 //BOJ_2747_피보나치 수 - dp
 
 import java.io.*;
 import java.util.*;
 public class BOJ_2747 {
+
+    public static int[] fibo;
+
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int testcase_num = Integer.parseInt(br.readLine());
-        int [] fibo = new int[46];
+        fibo = new int[46];
         fibo[0] = 0;
         fibo[1] = 1;
 
-        for (int i =2; i <= testcase_num; i++) {
+        int target = Integer.parseInt(br.readLine());
+        for(int i =2 ; i <= target; i++) {            
             fibo[i] = fibo[i - 1] + fibo[i - 2];
         }
-        System.out.println(fibo[testcase_num]);
 
+        System.out.println(fibo[target]);
+
+        
     }
 
 }
